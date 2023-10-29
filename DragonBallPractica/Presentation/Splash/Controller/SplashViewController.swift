632ef778e996d35244c08d18578ae26957dcb02d
Luis Herrera Lillo <lherreralillo@gmail.com>
2023-10-29 00:58:11 -0300

    Add SplashViewController and SplashViewState presentation model

---
+++ DragonBallPractica/Presentation/Splash/Controller/SplashViewController.swift
@@ -0,0 +1,51 @@
+//
+//  SplashViewController.swift
+//  DragonBallPractica
+//
+//  Created by Luis Eduardo Herrera Lillo on 29-10-23.
+//
+
+import UIKit
+
+// MARK: - Protocol
+protocol SplashViewControllerDelegate {
+    func onViewsLoaded()
+    var viewState: ((SplashViewState) -> Void)? { get set }
+}
+
+final class SplashViewController: UIViewController {
+    // MARK: - Outlets
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+    // MARK: - ViewModel
+    var viewModel: SplashViewControllerDelegate?
+    
+    // MARK: - LifeCycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setObservers()
+        viewModel?.onViewsLoaded()
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        activityIndicator.isHidden = true
+    }
+}
+
+// MARK: - Private Methods
+extension SplashViewController {
+    private func setObservers() {
+        viewModel?.viewState = { [weak self] state in
+            DispatchQueue.main.async {
+                switch state {
+                case let .loading(isLoading):
+                    self?.activityIndicator.isHidden = !isLoading
+                case .navigateToLogin:
+                    break
+                }
+            }
+        }
+    }
+}
---
+++ DragonBallPractica/Presentation/Splash/Models/SplashViewState.swift
@@ -0,0 +1,13 @@
+//
+//  SplashViewState.swift
+//  DragonBallPractica
+//
+//  Created by Luis Eduardo Herrera Lillo on 29-10-23.
+//
+
+import Foundation
+
+enum SplashViewState {
+    case loading(_ isLoading: Bool)
+    case navigateToLogin
+}
