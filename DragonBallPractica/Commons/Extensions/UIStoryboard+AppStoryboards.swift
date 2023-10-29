82189eb926cdb7729dd596615fa9760d7e9d2c41
Luis Herrera Lillo <lherreralillo@gmail.com>
2023-10-29 01:35:22 -0300

    Add UIStoryboard+AppStoryboards extension

---
+++ DragonBallPractica/Commons/Extensions/UIStoryboard+AppStoryboards.swift
@@ -0,0 +1,32 @@
+//
+//  UIStoryboard+AppStoryboards.swift
+//  DragonBallPractica
+//
+//  Created by Luis Eduardo Herrera Lillo on 29-10-23.
+//
+
+import UIKit
+
+extension UIStoryboard {
+    
+    /// The uniform place where we state all the storyboard we have in our application
+    enum AppStoryboard: String {
+        case splash       
+        
+        var filename: String {
+            return rawValue.capitalized
+        }
+    }
+    
+    
+    // MARK: - Convenience Initializers
+    convenience init(storyboard: AppStoryboard, bundle: Bundle? = nil) {
+        self.init(name: storyboard.filename, bundle: bundle)
+    }
+    
+    
+    // MARK: - Class Functions
+    class func storyboard(_ storyboard: AppStoryboard, bundle: Bundle? = nil) -> UIStoryboard {
+        return UIStoryboard(name: storyboard.filename, bundle: bundle)
+    }
+}
