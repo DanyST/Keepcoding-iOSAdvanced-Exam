71f6700fb16ac5831f53857c4e15ac68e3e19e3f
Luis Herrera Lillo <lherreralillo@gmail.com>
2023-10-29 02:40:18 -0300

    Add LoginViewState

---
+++ DragonBallPractica/Presentation/Login/Models/LoginViewState.swift
@@ -0,0 +1,15 @@
+//
+//  LoginViewState.swift
+//  DragonBallPractica
+//
+//  Created by Luis Eduardo Herrera Lillo on 29-10-23.
+//
+
+import Foundation
+
+enum LoginViewState {
+    case loading(_ isLoading: Bool)
+    case showErrorEmail(_ error: String)
+    case showErrorPassword(_ error: String)
+    case navigateToNext
+}
