602fed580df730bad266d7b46d109710a7c11080
Luis Herrera Lillo <lherreralillo@gmail.com>
2023-10-29 13:56:22 -0300

    Add HTTPMethod

---
+++ DragonBallPractica/Data/Api/NetworkProvider/HTTPMethod.swift
@@ -0,0 +1,13 @@
+//
+//  HTTPMethod.swift
+//  DragonBallPractica
+//
+//  Created by Luis Eduardo Herrera Lillo on 29-10-23.
+//
+
+import Foundation
+
+enum HTTPMethod: String {
+    case post = "POST"
+    case get = "GET"
+}
