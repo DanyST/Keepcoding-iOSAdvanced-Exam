1055f212cead7a5f44669f28ae186d4ba8325ca7
Luis Herrera Lillo <lherreralillo@gmail.com>
2023-11-21 23:44:37 -0300

    Add HeroesViewControllerDelegate

---
+++ DragonBallPractica/Presentation/Heroes/Controller/HeroesViewControllerDelegate.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+protocol HeroesViewControllerDelegate {
+    var viewState: ((HeroesViewState) -> Void)? { get set }
+    var title: String { get }
+    var dataCount: Int { get }
+    func onViewsLoaded()
+    func heroCellModel(at index: Int) -> HeroCellModel?
+    func onItemSelected(at index: Int)
+}
