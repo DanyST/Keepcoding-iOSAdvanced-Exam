47b2b42786c6d2a1d2fa8babee19560df43dbc6a
Luis Herrera Lillo <lherreralillo@gmail.com>
2023-11-21 22:25:38 -0300

    Add loginUseCase and loginValidatorUseCase as dependences to LoginViewModel and implement it

--- DragonBallPractica/Presentation/Login/ViewModel/LoginViewModel.swift
+++ DragonBallPractica/Presentation/Login/ViewModel/LoginViewModel.swift
@@ -1,51 +1,36 @@
-//
-//  LoginViewModel.swift
-//  DragonBallPractica
-//
-//  Created by Luis Eduardo Herrera Lillo on 29-10-23.
-//
-
 import Foundation
 
 final class LoginViewModel {
     // MARK: - Properties
     private var _viewState: ((LoginViewState) -> Void)?
-    private let apiDataSource: ApiDataSourceProtocol
+    private let loginValidatorUseCase: LoginValidatorUseCaseProtocol
+    private let loginUseCase: LoginUseCaseProtocol
     
-    init(apiDataSource: ApiDataSourceProtocol) {
-        self.apiDataSource = apiDataSource
+    // MARK: - Initialization
+    init(
+        loginUseCase: LoginUseCaseProtocol,
+        loginValidatorUseCase: LoginValidatorUseCaseProtocol
+    ) {
+        self.loginValidatorUseCase = loginValidatorUseCase
+        self.loginUseCase = loginUseCase
     }
 }
 
 // MARK: - Private Methods
 extension LoginViewModel {
     private func doLoginWith(email: String, password: String) {
-        apiDataSource.login(for: email, with: password) { [weak self] result in
+        loginUseCase.execute(for: email, with: password) { [weak self] result in
             defer {
                 self?.viewState?(.loading(false))
             }
             switch result {
-            case let .success(token):
-                print("token response: ", token)
+            case .success:
                 self?.viewState?(.navigateToNext)
             case let .failure(error):
                 print("Error: ", error)
                 self?.viewState?(.showErrorPassword("Invalid username and/or password. Please check your credentials and try again"))
             }
         }
-    }
-    
-    private func isValid(email: String?) -> Bool {
-        guard let email, !email.isEmpty else { return false }
-        
-        let regExMatchEmail = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
-        let predicateEmail = NSPredicate(format:"SELF MATCHES %@", regExMatchEmail)
-        return predicateEmail.evaluate(with: email)
-    }
-    
-    private func isValid(password: String?) -> Bool {
-        guard let password else { return false }
-        return !password.isEmpty && password.count >= 4
     }
 }
 
@@ -63,14 +48,15 @@ extension LoginViewModel: LoginViewControllerDelegate {
     func onLoginPressed(with email: String?, and password: String?) {
         viewState?(.loading(true))
         
-        DispatchQueue.global().async {
-            guard self.isValid(email: email) else {
+        DispatchQueue.global().async { [weak self] in
+            guard let self else { return }
+            guard self.loginValidatorUseCase.isValid(email: email) else {
                 self.viewState?(.loading(false))
                 self.viewState?(.showErrorEmail("Please enter a valid email"))
                 return
             }
             
-            guard self.isValid(password: password) else {
+            guard self.loginValidatorUseCase.isValid(password: password) else {
                 self.viewState?(.loading(false))
                 self.viewState?(.showErrorPassword("Please enter a valid password"))
                 return
