4011e5e83b8ec549763d3a9bcef459dd1e6a8a89
Luis Herrera Lillo <lherreralillo@gmail.com>
2023-11-21 22:23:47 -0300

    Add navigateToHome case to SplashViewState

--- DragonBallPractica/Presentation/Splash/Models/SplashViewState.swift
+++ DragonBallPractica/Presentation/Splash/Models/SplashViewState.swift
@@ -1,13 +1,7 @@
-//
-//  SplashViewState.swift
-//  DragonBallPractica
-//
-//  Created by Luis Eduardo Herrera Lillo on 29-10-23.
-//
-
 import Foundation
 
 enum SplashViewState {
     case loading(_ isLoading: Bool)
     case navigateToLogin
+    case navigateToHome
 }
