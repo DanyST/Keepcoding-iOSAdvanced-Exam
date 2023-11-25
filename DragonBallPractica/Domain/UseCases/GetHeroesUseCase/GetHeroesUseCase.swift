437296bb0ae979cf9e881e7eaed909981242cc75
Luis Herrera Lillo <lherreralillo@gmail.com>
2023-11-25 15:49:40 -0300

    Enhance the logic in the GetHeroesUseCase to check if local heroes data exists. If it does, return the local data; otherwise, fetch remote heroes.

--- DragonBallPractica/Domain/UseCases/GetHeroesUseCase/GetHeroesUseCase.swift
+++ DragonBallPractica/Domain/UseCases/GetHeroesUseCase/GetHeroesUseCase.swift
@@ -8,6 +8,26 @@ struct GetHeroesUseCase: GetHeroesUseCaseProtocol {
     }
     
     func execute(name: String?, completion: @escaping (Result<Heroes, ApiError>) -> Void) {
-        repository.getHeroes(withName: name, completion: completion)
+        let localHeroes = repository.getHeroesFromDatabase()
+        
+        if localHeroes.isEmpty {
+            fetchAndHandleRemoteHeroes(withName: name, completion: completion)
+        } else {
+            completion(.success(localHeroes))
+        }
+    }
+}
+
+private extension GetHeroesUseCase {
+    func fetchAndHandleRemoteHeroes(withName name: String?, completion: @escaping (Result<Heroes, ApiError>) -> Void) {
+        repository.getHeroes(withName: name) { result in
+            switch result {
+            case let .success(remoteHeroes):
+                repository.saveHeroesInLocalDatabase(remoteHeroes)
+                completion(.success(remoteHeroes))
+            case let .failure(error):
+                completion(.failure(error))
+            }
+        }
     }
 }
