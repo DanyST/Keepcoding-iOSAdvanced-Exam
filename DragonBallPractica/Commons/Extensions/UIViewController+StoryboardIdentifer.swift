0cfc5a75c680f6a7b99901cd9f9443cf6c01c986
Luis Herrera Lillo <lherreralillo@gmail.com>
2024-03-01 20:20:43 -0300

    Delete headers comments from UIViewController+StoryboardIdentifer

--- DragonBallPractica/Commons/Extensions/UIViewController+StoryboardIdentifer.swift
+++ DragonBallPractica/Commons/Extensions/UIViewController+StoryboardIdentifer.swift
@@ -1,10 +1,3 @@
-//
-//  UIViewController+StoryboardIdentifer.swift
-//  DragonBallPractica
-//
-//  Created by Luis Eduardo Herrera Lillo on 29-10-23.
-//
-
 import UIKit
 
 extension UIViewController : StoryboardIdentifiable {}
