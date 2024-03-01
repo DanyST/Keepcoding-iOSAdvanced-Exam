cee9ef351b68592085cabb255877c85d9723f738
Luis Herrera Lillo <lherreralillo@gmail.com>
2024-03-01 20:21:29 -0300

    Add NetworkConstants

---
+++ DragonBallPractica/Data/DataSources/Api/NetworkProvider/NetworkConstants.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+enum NetworkConstants {
+    static let baseURL = "https://dragonball.keepcoding.education/api"
+    
+    enum Header {
+        enum Key {
+            static let contentType = "Content-Type"
+            static let authorization = "Authorization"
+        }
+        
+        enum Value {
+            static let aplicationJson = "application/json; charset=utf-8"
+            static let basic = "Basic"
+            static let bearer = "Bearer"
+        }
+    }
+}
