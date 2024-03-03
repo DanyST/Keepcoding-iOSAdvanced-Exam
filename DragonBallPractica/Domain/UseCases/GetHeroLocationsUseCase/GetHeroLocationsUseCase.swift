3cfd8e7b8773ad351d90765c0c6f25ae61a89b43
Luis Herrera Lillo <lherreralillo@gmail.com>
2024-03-03 15:00:55 -0300

    Change GetHeroLocationsUseCase for get local heroLocations from database if exist or get it from api

--- DragonBallPractica/Domain/UseCases/GetHeroLocationsUseCase/GetHeroLocationsUseCase.swift
+++ DragonBallPractica/Domain/UseCases/GetHeroLocationsUseCase/GetHeroLocationsUseCase.swift
@@ -8,6 +8,28 @@ struct GetHeroLocationsUseCase: GetHeroLocationsUseCaseProtocol {
     }
     
     func execute(with hero: Hero, completion: @escaping (Result<[HeroLocation], ApiError>) -> Void) {
-        repository.getHeroLocations(by: hero, completion: completion)
+        let localHeroLocations = repository.getHeroLocationsFromDataBase(by: hero)
+        
+        if localHeroLocations.isEmpty {
+            fetchAndHandleRemoteHeroLocations(by: hero, completion: completion)
+        } else {
+            completion(.success(localHeroLocations))
+        }
     }
 }
+
+private extension GetHeroLocationsUseCase {
+    func fetchAndHandleRemoteHeroLocations(by hero: Hero, completion: @escaping (Result<[HeroLocation], ApiError>) -> Void) {
+                
+        repository.getHeroLocations(by: hero) { result in
+            switch result {
+            case let .success(remoteHeroLocations):
+                repository.saveHeroLocationsInLocalDatabase(remoteHeroLocations, to: hero)
+                completion(.success(remoteHeroLocations))
+            case let .failure(error):
+                completion(.failure(error))
+            }
+        }
+    }
+}
+
