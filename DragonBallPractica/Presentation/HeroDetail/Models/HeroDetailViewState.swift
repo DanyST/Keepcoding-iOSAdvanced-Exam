ef1fb7ec982598b70a1ff98d98fc9668a63be0d5
Luis Herrera Lillo <lherreralillo@gmail.com>
2024-03-03 15:16:49 -0300

    Change HeroDetailViewState with new updateMap case and remove updateHeroLocations case

--- DragonBallPractica/Presentation/HeroDetail/Models/HeroDetailViewState.swift
+++ DragonBallPractica/Presentation/HeroDetail/Models/HeroDetailViewState.swift
@@ -3,5 +3,5 @@ import Foundation
 enum HeroDetailViewState {
     case loading(_ isLoading: Bool)
     case updateHero(hero: Hero)
-    case updateHeroLocations(hero: Hero, heroLocations: HeroLocations)
+    case updateMap(heroAnnotations: [HeroAnnotation])
 }
