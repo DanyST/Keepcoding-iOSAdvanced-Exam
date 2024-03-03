4c8753c36df81ada5f38d19ff86fe80a11b26b58
Luis Herrera Lillo <lherreralillo@gmail.com>
2024-03-03 14:59:22 -0300

    Add addHeroLocations and getHeroLocations Methods to LocalDatabaseDataSource

--- DragonBallPractica/Data/DataSources/Local/DatabaseLocalDataSource/LocalDatabaseDataSource.swift
+++ DragonBallPractica/Data/DataSources/Local/DatabaseLocalDataSource/LocalDatabaseDataSource.swift
@@ -5,15 +5,21 @@ struct LocalDatabaseDataSource {
     private let coreDataStack: CoreDataStackProtocol
     private let heroDAOToDomainMapper: HeroDAOToDomainMapperProtocol
     private let heroToDAOMapper: HeroToDAOMapperProtocol
+    private let heroLocationDAOToDomainMapper: HeroLocationDAOToDomainMapperProtocol
+    private let heroLocationsToDAOMapper: HeroLocationsToDAOMapperProtocol
     
     init(
         coreDataStack: CoreDataStackProtocol = CoreDataStack.shared,
         heroDAOToDomainMapper: HeroDAOToDomainMapperProtocol = HeroDAOToDomainMapper(),
-        heroToDAOMapper: HeroToDAOMapperProtocol = HeroToDAOMapper()
+        heroToDAOMapper: HeroToDAOMapperProtocol = HeroToDAOMapper(),
+        heroLocationDAOToDomainMapper: HeroLocationDAOToDomainMapperProtocol = HeroLocationDAOToDomainMapper(),
+        heroLocationsToDAOMapper: HeroLocationsToDAOMapperProtocol = HeroLocationsToDAOMapper()
     ) {
         self.coreDataStack = coreDataStack
         self.heroDAOToDomainMapper = heroDAOToDomainMapper
         self.heroToDAOMapper = heroToDAOMapper
+        self.heroLocationDAOToDomainMapper = heroLocationDAOToDomainMapper
+        self.heroLocationsToDAOMapper = heroLocationsToDAOMapper
     }
 }
 
@@ -46,6 +52,52 @@ extension LocalDatabaseDataSource: LocalDatabaseDataSourceProtocol {
             return heroesDAO.map { heroDAOToDomainMapper.map($0) }
         } catch {
             return []
+        }
+    }
+    
+    func add(heroLocations: HeroLocations, to hero: Hero) {
+        guard !heroLocations.isEmpty else { return }
+        let context = coreDataStack.persistentContainer.viewContext
+                
+        do {
+            guard let heroDAO = getHeroDAO(by: hero.id) else {
+                assertionFailure("Error fetching heroDAO")
+                return
+            }
+            
+            _ = heroLocationsToDAOMapper.map(heroLocations, heroDAO: heroDAO, context: context)
+                       
+            try context.save()
+        } catch {
+            assertionFailure("Error saving context")
+        }
+    }
+    
+    func getHeroLocations(by hero: Hero) -> HeroLocations {
+        let context = coreDataStack.persistentContainer.viewContext
+        let fetchRequest = HeroLocationDAO.fetchRequest()
+        fetchRequest.predicate = NSPredicate(format: "hero.id = %@", hero.id)
+        
+        do {
+            let heroLocationsDAO = try context.fetch(fetchRequest)
+            return heroLocationsDAO.map { heroLocationDAOToDomainMapper.map($0) }
+        } catch {
+            return []
+        }
+    }
+}
+
+private extension LocalDatabaseDataSource {
+    func getHeroDAO(by id: String) -> HeroDAO? {
+        let context = coreDataStack.persistentContainer.viewContext
+        let fetchRequest = HeroDAO.fetchRequest()
+        fetchRequest.predicate = NSPredicate(format: "id = %@", id)
+        
+        do {
+            let heroDAO = try context.fetch(fetchRequest).first
+            return heroDAO
+        } catch {
+            return nil
         }
     }
 }
--- DragonBallPractica/Data/DataSources/Local/DatabaseLocalDataSource/LocalDatabaseDataSourceProtocol.swift
+++ DragonBallPractica/Data/DataSources/Local/DatabaseLocalDataSource/LocalDatabaseDataSourceProtocol.swift
@@ -4,4 +4,6 @@ protocol LocalDatabaseDataSourceProtocol {
     func add(hero: Hero)
     func add(heroes: Heroes)
     func getHeroes() -> Heroes
+    func add(heroLocations: HeroLocations, to: Hero)
+    func getHeroLocations(by: Hero) -> HeroLocations
 }
