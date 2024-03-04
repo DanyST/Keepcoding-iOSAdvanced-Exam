c3c1f94215e7346cd402e716f4bd8b33a4eeecd9
Luis Herrera Lillo <lherreralillo@gmail.com>
2024-03-03 21:16:31 -0300

    Fix HeroDTOToDomainMapper for create photoUrlComponents only if photo has a value

--- DragonBallPractica/Data/DataSources/Api/Mappers/HeroDTOToDomainMapper/HeroDTOToDomainMapper.swift
+++ DragonBallPractica/Data/DataSources/Api/Mappers/HeroDTOToDomainMapper/HeroDTOToDomainMapper.swift
@@ -3,8 +3,12 @@ import Foundation
 struct HeroDTOToDomainMapper: HeroDTOToDomainMapperProtocol {
     func map(_ heroDTO: HeroDTO) -> Hero? {
         guard let id = heroDTO.id else { return nil }
-        
-        var photoUrlComponents = URLComponents(string: heroDTO.photo ?? "")
+
+        var photoUrlComponents: URLComponents? = if let photo = heroDTO.photo {
+            URLComponents(string: photo)
+        } else {
+            nil
+        }
         photoUrlComponents?.scheme = "https"
         
         return Hero(
